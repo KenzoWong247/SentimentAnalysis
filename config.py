12b69b65096909e115198b6a262af3faa56727ea
Kenzo Wong <wongk8@wit.edu>
2018-08-09 09:51:51 -0400

    config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'kenzo-wongs-secret-key-whahaha'
+    SECRET_KEY = 'kenzo-wong-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
