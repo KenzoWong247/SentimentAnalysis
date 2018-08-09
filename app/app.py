f529921bab3d8c1bee523826e222e89b61da59eb
Kenzo Wong <wongk8@wit.edu>
2018-08-09 08:05:38 -0400

    Updated pipfile

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config.from_object(Config)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
