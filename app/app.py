69b87b782d9d113c886b028d3ad6dc8b813e9d51
Kenzo Wong <wongk8@wit.edu>
2018-08-09 07:50:22 -0400

    Updated pip

--- app/app.py
+++ app/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 import os
-from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
