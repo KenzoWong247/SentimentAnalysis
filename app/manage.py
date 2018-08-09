98d7bfba3ad8402f0d9e8b47bd98541c3bc5f74c
Kenzo Wong <wongk8@wit.edu>
2018-08-09 09:19:00 -0400

    Changed myapp to app and changed app to start

--- app/manage.py
+++ app/manage.py
@@ -2,13 +2,13 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from myapp import app, db
+from app import run, db
 
 
-app.config.from_object(os.environ['APP_SETTINGS'])
+run.config.from_object(os.environ['APP_SETTINGS'])
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+migrate = Migrate(run, db)
+manager = Manager(run)
 
 manager.add_command('db', MigrateCommand)
 
--- app/model.py
+++ app/model.py
@@ -1,4 +1,4 @@
-from myapp import db
+from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
 
